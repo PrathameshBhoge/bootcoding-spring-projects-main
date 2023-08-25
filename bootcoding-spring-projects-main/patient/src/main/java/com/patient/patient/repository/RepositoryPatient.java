67cae89fe2f51e0e6b306f42995971bf0c72210b
PrathameshBhoge <prathameshbhoge.cool@gmail.com>
2023-08-25 12:26:05 +0530

    Patient App Random Generator

--- bootcoding-spring-projects-main/patient/src/main/java/com/patient/patient/repository/RepositoryPatient.java
+++ bootcoding-spring-projects-main/patient/src/main/java/com/patient/patient/repository/RepositoryPatient.java
@@ -1,5 +1,10 @@
 package com.patient.patient.repository;
 
-public class RepositoryPatient {
+import com.patient.patient.Model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositoryPatient extends JpaRepository<Patient,Integer> {
 
 }
